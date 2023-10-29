687d143c31fed457f4550c44a3b8d1551ff75fd5
devbutlazy <devbutlazy@gmail.com>
2023-10-29 17:28:34 +0200

    Token hidden

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from src import bot
 from os import getenv
 
-bot.Bot().run(
-    "MTE2NzQ4NTY5OTAxNzE2NjkwOA.GwErVG.OGRdKpA-a2u8tO3F9JIeyRcBNYczXiNftUKEpw"
-)
+bot.Bot().run(getenv("DISCORD_TOKEN", "token"))
