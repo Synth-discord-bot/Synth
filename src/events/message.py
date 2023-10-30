5b292311bebc806f7e4bcb8a4993fb0f36dab6e0
Snaky <snaky1a@outlook.com>
2023-10-30 13:29:30 +0200

    Update 0.3:
    [Base database]: Update base class
    [Logger & database]: Implement create_logger. Update updated_logger_info functions and rewritten logger file
    [Other]: reformatted code, restructured logger events to "Event" folder

--- src/events/message.py
+++ src/events/message.py
@@ -1,10 +1,120 @@
+from datetime import datetime
+
+import disnake
 from disnake.ext import commands
+from src.utils import logger
 
 
 class EventMessages(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         super(EventMessages, self).__init__()
+        self.logger = logger
         self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message_delete(self, message: disnake.Message) -> None:
+        if message.author == message.guild.me:
+            return
+
+        message.content = (
+            message.content[:1900] + "..."
+            if len(message.content) > 1900
+            else message.content
+        )  # TODO: send message to text file if message.content > 1900
+        embeds = []
+        embed = disnake.Embed(color=0x2F3136, title="Deleted Message", description=None)
+        embed.add_field(name="Additional information", value="No")
+        field_dop_index = next(
+            index
+            for index, field in enumerate(embed.fields)
+            if field.name == "Additional information"
+        )
+        files = []
+        embeds.append(embed)
+
+        if message.attachments:
+            for num, attachment in enumerate(message.attachments, start=1):
+                embed.set_field_at(
+                    field_dop_index,
+                    name="Additional information",
+                    value=f"Number of media files: `{num}`",
+                )
+                files.append(await attachment.to_file())
+
+        if message.embeds:
+            embed.set_field_at(
+                field_dop_index,
+                name="Additional information",
+                value=f"Number of Embeds Deleted: `{len(message.embeds)}`",
+            )
+            for num, emb in enumerate(message.embeds, start=1):
+                if num < 9:
+                    emb.set_footer(text=f"Number: {num}")
+                    embeds.append(emb)
+
+        embed.add_field(
+            name="Author",
+            value=f"{message.author.mention} (`{message.author}` `ID: {message.author.id}`)",
+            inline=False,
+        )
+        embed.add_field(
+            name="Deleted at",
+            value=disnake.utils.format_dt(datetime.now(), style="f"),
+            inline=True,
+        )
+        embed.add_field(
+            name="Channel",
+            value=f"{message.channel.mention} (`ID: {message.channel.id}`)",
+        )
+        embed.set_thumbnail(url=message.author.avatar)
+        logger_channel = await self.logger.get_loggers(
+            guild_id=message.guild.id, to_return="message"
+        )
+        channel = message.guild.get_channel(int(logger_channel))
+        await channel.send(embeds=embeds, files=files)
+
+    @commands.Cog.listener()
+    async def on_message_edit(self, before, after):
+        if before.author == before.guild.me:
+            return
+
+        if before.content == after.content:
+            return
+
+        before.content = (
+            before.content[:1900] + "..."
+            if len(before.content) > 1900
+            else before.content
+        )  # TODO: send message to text file if before.content > 1900
+        after.content = (
+            after.content[:1900] + "..." if len(after.content) > 1900 else after.content
+        )  # TODO: send message to text file if after.content > 1900
+        if before.content == after.content:
+            return
+        embed = disnake.Embed(
+            title="Edited Message",
+            description=f"Before: {before.content}\nAfter: {after.content}",
+            color=0x2F3136,
+        )
+        embed.add_field(
+            name="Author",
+            value=f"{after.author.mention} (`{after.author}` `ID: {after.author.id}`)",
+            inline=False,
+        )
+        embed.add_field(
+            name="Edited at",
+            value=disnake.utils.format_dt(datetime.now(), style="f"),
+            inline=True,
+        )
+        embed.add_field(
+            name="Channel",
+            value=f"{before.channel.mention} (`ID: {before.channel.id}`)",
+        )
+        logger_channel = await self.logger.get_loggers(
+            guild_id=before.guild.id, to_return="message"
+        )
+        channel = before.guild.get_channel(int(logger_channel))
+        await channel.send(embed=embed)
 
 
 def setup(bot: commands.Bot) -> None:
--- src/utils/database/logger.py
+++ src/utils/database/logger.py
@@ -1,4 +1,4 @@
-from typing import Any, List, Mapping, Union
+from typing import Any, List, Mapping, Union, Dict
 
 import disnake
 
@@ -12,24 +12,29 @@ class LoggerDatabase(BaseDatabase):
     async def get_loggers(
         self,
         guild_id: Union[int, str, disnake.Guild],
-        return_first_logger: bool = False,
-    ) -> List[Mapping[Any, Any]]:
+        to_return: str = None,
+    ) -> Union[List[Mapping[Any, Any]], int, Dict[str, str]]:
         guild_id = int(guild_id) if isinstance(guild_id, (str, int)) else guild_id.id
 
-        if await self.find_one_from_db({"guild_id": guild_id}) is None:
+        if (
+            await self.find_one({"guild_id": guild_id}, return_first_result=True)
+            is None
+        ):
             return []
-        result = (await self.find_one_from_db({"guild_id": guild_id})).get(
-            "loggers", []
-        )
-        if return_first_logger and result:
-            return result[0]
-        return result
+
+        result = (
+            await self.find_one({"guild_id": guild_id}, return_first_result=True)
+        ).get("loggers", [])
+        if to_return and result:
+            return result[0].get(to_return, "")
+        return result[0]
 
     async def update_logger_info(
         self,
         guild_id: Union[int, str, disnake.Guild],
-        logger_channel_id: int,
-    ) -> int:
+        logger_type: str = None,
+        logger_channel_id: str = None,
+    ) -> Dict[str, str]:
         if await self.find_one_from_db({"guild_id": guild_id}) is None:
             await self.add_to_db(
                 {
@@ -38,13 +43,50 @@ class LoggerDatabase(BaseDatabase):
                 }
             )
 
+        if not logger_type and not logger_channel_id:
+            raise Exception(
+                "You must provide a logger type (string like message_edit) and/or a logger channel id"
+            )
+
         loggers = await self.get_loggers(guild_id=guild_id)
-        loggers.append(
-            {
-                "logger_channel_id": logger_channel_id,
-            }
-        )
+
+        # check if "key" exists in DB
+        if logger_type not in loggers:
+            loggers.append({logger_type: logger_channel_id})
+        else:
+            loggers.update({logger_type: logger_channel_id})
+
         await self.update_db(
             {"guild_id": guild_id},
             {"loggers": loggers},
+        )
+        return {logger_type: logger_channel_id}
+
+    async def create_logger(
+        self,
+        guild_id: Union[int, str, disnake.Guild],
+        main_log_channel: disnake.TextChannel,
+        guild_log_channel: disnake.TextChannel = None,
+        invite_log_channel: disnake.TextChannel = None,
+        message_log_channel: disnake.TextChannel = None,
+    ) -> None:
+        guild_id = (
+            guild_id.id
+            if isinstance(guild_id, disnake.Guild)
+            else int(guild_id)
+            if isinstance(guild_id, str)
+            else guild_id
+        )
+        return await self.update_db(
+            {"guild_id": guild_id},
+            {
+                "loggers": [
+                    {
+                        "main": main_log_channel.id,
+                        "message": message_log_channel.id,
+                        "invite": invite_log_channel.id,
+                        "guild": guild_log_channel.id,
+                    },
+                ]
+            },
         )
