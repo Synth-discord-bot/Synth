3cea5200a70db9e6f380b4118bb6a84877a3579c
devbutlazy <devbutlazy@gmail.com>
2023-12-31 21:29:12 +0200

    Just added DevCog

---
+++ src/cogs/DevCog.py
@@ -0,0 +1,18 @@
+import disnake
+from disnake.ext import commands
+from traceback import format_exception
+import contextlib
+import io
+import textwrap
+
+class Developers(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        super(Developers, self).__init__()
+        self.bot = bot
+
+    #TODO: EVAL WITHOUGHT INTERNET
+    #TODO: BLACKLIST SETTINGS
+    #TODO: COGS LOAD/UNLOAD/RELOAD
+
+def setup(bot: commands.Bot) -> None:
+    bot.add_cog(Developers(bot))
