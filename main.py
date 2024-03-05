b8f9f06a65bc67ef28e8bf10d501642c621d3ebb
Snaky <snaky1awq@gmail.com>
2024-03-05 16:42:05 +0200

    q

--- main.py
+++ main.py
@@ -4,6 +4,5 @@ from src import bot
 
 if __name__ == "__main__":
     debug = bool(getenv("DEBUG", False))
-    token = "MTE2ODIyNjg5NTc1ODkwMTQwOA.GCNd9-.Qgk_m-7atWUxb73e4LMmB564bUyPP8w0xlmTX4"
 
-    bot.Bot(debug=debug).run(token)
+    bot.Bot(debug=debug).run(getenv("TOKEN"))
